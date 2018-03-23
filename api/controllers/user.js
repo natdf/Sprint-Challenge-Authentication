0ffb2e112c1fac2335b61014b4ce67c413bde2b8
Nathan Daniel <natdf@icloud.com>
2018-03-23 11:43:59 -0500

    Commit 2 * user.js - implemented create user functionality - new user successfully created * app.js - added messages verifying connection to mongodb

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,10 +3,15 @@ const bcrypt = require('bcrypt');
 
 const createUser = (req, res) => {
   const { username, password } = req.body;
+  const user = new User({ username, password: password });
+  user.save((err, user) => {
+    if (err) return res.send(err);
+    res.json({ success: 'User saved', user });
+  });
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
 };
 
 module.exports = {
-  createUser
+  createUser,
 };
--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/auth-users', {
-  useMongoClient: true
-});
+mongoose
+  .connect('mongodb://localhost/auth-users', { useMongoClient: true })
+  .then(() => {
+    console.log('Successfully connected to MongoDB');
+  })
+  .catch(err => {
+    console.log('Error connecting to database: ', err);
+  });
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
